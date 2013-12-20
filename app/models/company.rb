15836fb70c1932c8da0c647dba1223055d8dcdc8
vagrant <vagrant@rails-dev-box>
2013-12-20 04:30:52 +0000

    add model files

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,10 @@ class Company < ActiveRecord::Base
   has_many :groups
   validates :url, presence: true
   validates :url, length: {maximum: 30}, allow_nil: true, allow_blank: true 
-
+  validates :adress, presence: true
+  validates :adress, length: { maximum: 21 }
+  validates :tel, format: { with: /[0-9]{1,4}-[0-9]{1,4}-[0-9]{1,4}/ } 
+  validates :tel, length: { maximum: 11 }, allow_nil: true
+  validates :fax, format: { with: /[0-9]{1,4}-[0-9]{1,4}-[0-9]{1,4}/ }
+  validates :fax, length: { maximum: 11 }, allow_nil: true
 end
